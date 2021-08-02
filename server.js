d2ffdab24005ce0da8bda285a495aca1e5d47bca
echcherrate.ismail <echcherrate.ismail@gmail.com>
2021-08-02 02:01:35 +0100

    C:/Program Files/Git/api/fileanalyse endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +8,22 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+
+    console.log(req.file)
+    let uploadedFile = req.file;
+    return res.json({ "name": uploadedFile.originalname, "type": uploadedFile.mimetype, "size": uploadedFile.size });
+
+});
+
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function() {
+    console.log('Your app is listening on port ' + port)
 });
